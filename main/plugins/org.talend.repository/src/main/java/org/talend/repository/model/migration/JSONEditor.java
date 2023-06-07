db8a0a8bdb01a887ac64967b4ec9004a36851d0d
Abdulhamid Rashankar <arashankar@talend.com>
2023-06-07 13:03:15 +0200

    APPINT-35640:Studio:Data Usage: remove old nb.route.osgi under process in Data Collector

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/JSONEditor.java
@@ -0,0 +1,12 @@
+package org.talend.repository.model.migration;
+
+import us.monoid.json.JSONException;
+import us.monoid.json.JSONObject;
+
+public class JSONEditor {
+
+	public static void main(String args[]) throws JSONException {
+		
+		
+	}
+}
